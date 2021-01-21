b62ce1f2a2e84ce1b972d16e3fad783a5b3cb947
EC2 Default User <ec2-user@ip-172-31-13-253.ap-northeast-2.compute.internal>
2021-01-21 23:41:50 +0000

    added

---
+++ 00_install-prereq.sh
@@ -0,0 +1,135 @@
+    1  python
+    2  python version
+    3  python3
+    4  which python
+    5  cd usr/bin
+    6  cd /usr/bin
+    7  ls -al
+    8  ls -al python
+    9  ls -al python*
+   10  python3 --version
+   11  sudo yum list | grep python3
+   12  sudo yum install python34 python37-pip
+   13  sudo yum install python37 python37-pip
+   14  sudo amazon-linux-extras install python3
+   15  python version
+   16  python3
+   17  python3 -m venv python3
+   18  pyenv
+   19  which python3
+   20  which python3.7
+   21  sudo /usr/bin/python3.7 awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+   22  vi .bashrc
+   23  cd
+   24  vi .bashrc
+   25  . .bashrc
+   26  python
+   27  . .bashrc
+   28  vi .bashrc
+   29  . .bashrc
+   30  python
+   31  whcih python3
+   32  which python3
+   33  /usr/bin/python3
+   34  vi ~/.bashrc
+   35  . .bashrc
+   36  python3
+   37  ll
+   38  ls -al
+   39  python version
+   40  python3
+   41  cd /usr/bin/
+   42  ls -al
+   43  ls -al python*
+   44  python3
+   45  cd
+   46  vi ~/.bashrc
+   47  . ~/.bashrc
+   48  python3 --version
+   49  pip3
+   50  yum update
+   51  pyenv
+   52  pip3 install requests bs4 python-telegram-bot apscheduler
+   53  pip3 install --user requests bs4 python-telegram-bot apscheduler
+   54  ls -al
+   55  cat scrapper.py
+   56  ls -al
+   57  mv scrapper.py movie-scrapper.py
+   58  ls -al
+   59  mkdir projects
+   60  mv movie-scrapper.py projects/
+   61  ll
+   62  ls -al
+   63  cd projects/
+   64  ll
+   65  python movie-scrapper.py
+   66  ps -ef | grep -i sched
+   67  python3 movie-scrapper.py
+   68  ls -al
+   69  vi start.sh
+   70  bash start.sh
+   71  ls -al
+   72  cat nohup.out
+   73  vi start.sh
+   74  ls -al
+   75  rm nohup.out
+   76  bash start.sh
+   77  ll
+   78  cat nohup.out
+   79  ps -ef| grep -i nohup
+   80  ps -ef | grep -i movie
+   81  ll
+   82  ps -ef | grep nohup
+   83  ps -ef | grep -i nohup
+   84  ps
+   85  ls -al
+   86  cat start.sh
+   87  nohup python3 movie-scrapper.py &
+   88  ps
+   89  ls -al
+   90  python3 movie-scrapper.py
+   91  cat movie-scrapper.py
+   92  nohup
+   93  cd ..
+   94  ls -al
+   95  mkdir platform
+   96  ls -al
+   97  cd platform/
+   98  ls -al
+   99  git clone https://github.com/jupyterbook/minikube.git
+  100  yum install git
+  101  sudo yum install git
+  102  git clone https://github.com/jupyterbook/minikube.git
+  103  ls -al
+  104  cd minikube/
+  105  ls -al
+  106  cat 01_deploy-minikube.sh
+  107  docker ps
+  108  ls -al
+  109  minikube
+  110  cat 01_deploy-minikube.sh
+  111  curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64   && chmod +x minikube
+  112  sudo cp minikube /usr/local/bin && rm minikube
+  113  minikube
+  114  docker
+  115  sudo yum update -y
+  116  sudo amazon-linux-extras install docker
+  117  sudo service docker start
+  118  sudo usermod -a -G docker ec2-user
+  119  docker info
+  120  exit
+  121  docker info
+  122  ls -al
+  123  cd platform/
+  124  ll
+  125  mv minikube/ ..
+  126  cd ..
+  127  ls -al
+  128  rm -rf platform/
+  129  ll
+  130  cd minikube/
+  131  ll
+  132  cat 01_deploy-minikube.sh
+  133  vi 01_deploy-minikube.sh
+  134  bash 01_deploy-minikube.sh
+
--- 01_deploy-minikube.sh
+++ 01_deploy-minikube.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #minikube start --cpus 6 --memory 12288 --disk-size=120g --extra-config=apiserver.service-account-issuer=api --extra-config=apiserver.service-account-signing-key-file=/var/lib/minikube/certs/apiserver.key --extra-config=apiserver.service-account-api-audiences=api
 
-minikube start --vm-driver=docker --cpus 6 --memory 12288 --disk-size=120g --feature-gates=DevicePlugins=true --extra-config=apiserver.service-account-issuer=api --extra-config=apiserver.service-account-signing-key-file=/var/lib/minikube/certs/apiserver.key --extra-config=apiserver.service-account-api-audiences=api
+minikube start --vm-driver=docker --feature-gates=DevicePlugins=true --extra-config=apiserver.service-account-issuer=api --extra-config=apiserver.service-account-signing-key-file=/var/lib/minikube/certs/apiserver.key --extra-config=apiserver.service-account-api-audiences=api
 
 #minikube addons enable nvidia-gpu-device-plugin
 #minikube addons enable nvidia-driver-installer
